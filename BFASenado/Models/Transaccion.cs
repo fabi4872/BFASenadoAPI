449bbd7e7a142de481bf4bb025588341f8e7580a
Fabian Salomone <fabi4872@gmail.com>
2024-11-14 07:47:06 -0300

    Creacion de API

---
+++ BFASenado/Models/Transaccion.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+namespace BFASenado.Models
+{
+    [Table("Transaccion")]
+    public partial class Transaccion
+    {
+        [Key]
+        public long Id { get; set; }
+        [StringLength(100)]
+        [Unicode(false)]
+        public string Hash { get; set; } = null!;
+        [Column(TypeName = "text")]
+        public string Base64 { get; set; } = null!;
+    }
+}
